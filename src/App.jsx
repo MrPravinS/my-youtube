0f94c82f483173c35e2df01ef8e19686046e0219
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-28 01:05:02 +0530

    building components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import './App.css'
+import Head from './components/Head'
+import Body from './components/Body'
 
 function App() {
 
   return (
    <div>
-
+     <h1 className='text-3xl'>Building Youtube</h1>
+     <Head/>
+     <Body/>
+     {/* {
+       head 
+       body
+       sidebar
+          menubar
+        maincontainer
+        buttonlist
+        videocontainer
+           videocard
+       } */}
    </div>
   )
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from './SideBar'
+import MainContainer from './MainContainer'
+
+const Boady = () => {
+  return (
+    <div>
+        <SideBar/>
+        <MainContainer/>
+    </div>
+  )
+}
+
+export default Boady
