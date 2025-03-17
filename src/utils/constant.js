ad4d1ac02af091a5ee94f0a7c14c0a2d37bcad86
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-03-17 15:05:54 +0530

    Stop tracking constant.js

--- src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-const API_KEY = "AIzaSyAXgE-YmlcxYIziBtJ-gQnoogPOUgZSX1M"
-
-export const YOUTUBE_VIDEOS_API = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`
-
-
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
